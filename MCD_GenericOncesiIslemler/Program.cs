f5f87e64052bbc4cc1bb25d787d6b70917d01996
GAMZE KURAL <gamzekural98@gmail.com>
2021-12-07 17:11:03 +0300

    Add project files.

---
+++ MCD_GenericOncesiIslemler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MCD_GenericOncesiIslemler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList liste = new ArrayList();
+            //liste.Add();
+
+            SanalDataBase sdatabase = new SanalDataBase();
+            sdatabase.yeniKayit(1);
+            sdatabase.yeniKayit(2);
+        }
+    }
+}
---
+++ MCD_GenericOncesiIslemler/SanalDataBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MCD_GenericOncesiIslemler
+{
+    public class SanalDataBase
+    {
+        private ArrayList listem;
+
+        public SanalDataBase()
+        {
+            listem = new ArrayList();
+        }
+
+        public void yeniKayit(int data)
+        {
+            listem.Add(listem);
+        }
+    }
+}
